e4bbaf0e6edee4e5b2930f83ebf9b28bed7e6206
toshiki31 <mori.toshiki@arakawa-lab.com>
2026-03-20 16:10:14 +0900

    fix: fetch系関数にHTTPエラー時の例外throwを追加する

--- application/client/src/utils/fetchers.ts
+++ application/client/src/utils/fetchers.ts
@@ -2,22 +2,23 @@ import { gzip } from "pako";
 
 export async function fetchBinary(url: string): Promise<ArrayBuffer> {
   const response = await fetch(url);
+  if (!response.ok) throw new Error(`HTTP ${response.status}`);
   return response.arrayBuffer();
 }
 
 export async function fetchJSON<T>(url: string): Promise<T> {
   const response = await fetch(url);
+  if (!response.ok) throw new Error(`HTTP ${response.status}`);
   return response.json();
 }
 
 export async function sendFile<T>(url: string, file: File): Promise<T> {
   const response = await fetch(url, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/octet-stream",
-    },
+    headers: { "Content-Type": "application/octet-stream" },
     body: file,
   });
+  if (!response.ok) throw new Error(`HTTP ${response.status}`);
   return response.json();
 }
 
@@ -34,5 +35,6 @@ export async function sendJSON<T>(url: string, data: object): Promise<T> {
     },
     body: compressed,
   });
+  if (!response.ok) throw new Error(`HTTP ${response.status}`);
   return response.json();
 }
